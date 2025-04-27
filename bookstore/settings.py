ed69430b02d6acca5d79dca1ef669ff5964be8e5
Alison Oliveira <alison.oliveira89@gmail.com>
2025-04-26 23:29:55 -0300

    update allowed_hosts

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -147,5 +147,5 @@ DEBUG = int(os.environ.get("DEBUG", default=0))
 # 'DJANGO_ALLOWED_HOSTS' should be a single string of hosts with a space between each.
 # For example: 'DJANGO_ALLOWED_HOSTS=localhost 127.0.0.1 [::1]'
 # ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "127.0.0.1").split(" ")
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'aosdev.pythonanywhere.com']
+ALLOWED_HOSTS = ['aosdev.pythonanywhere.com']
 
