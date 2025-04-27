c0cca680b17782cea74f73373173975baa0bded3
Alison Oliveira <alison.oliveira89@gmail.com>
2025-04-26 23:11:35 -0300

    add pythonanywhere on hosts

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -146,5 +146,6 @@ DEBUG = int(os.environ.get("DEBUG", default=0))
 
 # 'DJANGO_ALLOWED_HOSTS' should be a single string of hosts with a space between each.
 # For example: 'DJANGO_ALLOWED_HOSTS=localhost 127.0.0.1 [::1]'
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "127.0.0.1").split(" ")
+# ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "127.0.0.1").split(" ")
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'aosdev.pythonanywhere.com']
 
