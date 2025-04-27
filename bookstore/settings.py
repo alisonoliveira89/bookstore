8b99736085db22df4e9c3236abd6e1152de0601f
Alison Oliveira <alison.oliveira89@gmail.com>
2025-04-27 18:35:03 -0300

    conf hosts pythonanywhere

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-o01r+mhp9nbmx$ub!m))h=zxx@_8y)l1^qp0@minlb!dq3+5c%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://aosdev.pythonanywhere.com']
 
 
 # Application definition
@@ -153,4 +153,4 @@ DEBUG = int(os.environ.get("DEBUG", default=0))
 
 # 'DJANGO_ALLOWED_HOSTS' should be a single string of hosts with a space between each.
 # For example: 'DJANGO_ALLOWED_HOSTS=localhost 127.0.0.1 [::1]'
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "").split(" ")
+#ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "").split(" ")
